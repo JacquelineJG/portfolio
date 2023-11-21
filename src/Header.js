98b297dfd4c4d3a21f194636daea1b9f373e8e22
Jacqueline <jacqueline.mjg@hotmail.com>
2023-11-20 20:15:43 -0500

    header logo about

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,14 @@ function Header() {
     <div>
       <header class="page-header">
         <img class="logo" src={logo} alt=""/>
-        <p>Hi</p>
+        <nav>
+          <ul class="main-nav">
+            <li class="list-item">Home</li>
+            <li class="list-item">Projects</li>
+            <li class="list-item">Resume</li>
+            <li class="list-item">About Me</li>
+          </ul>
+        </nav>
         </header>
     </div>
   );
